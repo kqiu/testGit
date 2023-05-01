ddef8111a7d796a2828c931bf3adff40ff642162
kaiqiu <692813824@qq.com>
2023-05-01 10:13:47 +0800

    1. 提交dev-v2

--- test.js
+++ test.js
@@ -2,6 +2,11 @@ const c = new Promise((resolve, reject)=>{
     resolve('a')
 });
 
-const res = await c;
+// 删除部分 in dev-v2;
+// const res = await c;
 
-console.log('res>>>', res)
+console.log('修改部分 in dev-v2>>>res>>>', res)
+
+
+/*****新增dev-v2********/
+console.log("新增部分in test.js");
--- test2.js
+++ test2.js
@@ -1,4 +1,9 @@
-console.log("test2********************")
+console.log("修改部分In dev-v2>>>test2********************")
 
-const test2 = 1;
+// 删除部分in dev-v2
+// const test2 = 1
+
+/******新增in dev-v2***********/
+const newInDevv2 = '111';
+
 
